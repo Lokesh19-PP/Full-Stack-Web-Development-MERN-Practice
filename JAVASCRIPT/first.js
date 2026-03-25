907c827cfe9dff2faafb2d7eacf39d992d67fbb8
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-03-25 23:10:41 +0530

    learn about the swith statemnet in js

--- JAVASCRIPT/first.js
+++ JAVASCRIPT/first.js
@@ -46,30 +46,58 @@
 //   }
 // }
 
-let a = 10,
-  b = 20,
-  c = 25;
+// let a = 10,
+//   b = 20,
+//   c = 25;
 
-if (a > b && a > c) {
-  console.log("a is greater");
-} else if (b > a && b > c) {
-  console.log("b is grater");
-} else {
-  console.log("c is grater");
-}
+// if (a > b && a > c) {
+//   console.log("a is greater");
+// } else if (b > a && b > c) {
+//   console.log("b is grater");
+// } else {
+//   console.log("c is grater");
+// }
 
-let age = 70;
+// let age = 70;
 
-if (age < 5 || age >= 65) {
-  console.log("free");
-} else if (age < 10 || age <= 18) {
-  console.log("$10");
-} else {
-  comsole.log("$20");
-}
+// if (age < 5 || age >= 65) {
+//   console.log("free");
+// } else if (age < 10 || age <= 18) {
+//   console.log("$10");
+// } else {
+//   comsole.log("$20");
+// }
 
-let fname = prompt("enter your name : ");
+// let fname = prompt("enter your name : ");
 
-if (!fname) {
-  name = prompt("try again!!");
+// if (!fname) {
+//   name = prompt("try again!!");
+// }
+
+let day = 3;
+
+switch (day) {
+  case 1:
+    console.log("monday");
+    break;
+  case 2:
+    console.log("tuesday");
+    break;
+  case 3:
+    console.log("wednesday");
+    break;
+  case 4:
+    console.log("thursday");
+    break;
+  case 5:
+    console.log("friday");
+    break;
+  case 6:
+    console.log("satuday");
+    break;
+  case 7:
+    console.log("sunday");
+    break;
+  default:
+    console.log("not valid....");
 }
