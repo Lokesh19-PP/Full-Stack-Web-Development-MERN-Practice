fca8818a2141c0c46e8141325171c1516a4ddf2b
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-03-25 22:36:54 +0530

    learn about the if-else statement in js

--- JAVASCRIPT/first.js
+++ JAVASCRIPT/first.js
@@ -1,6 +1,13 @@
 // console.log("Hello from the first JS file!!!!");
 
-let rating = 3;
-if (rating === 3) {
-  console.log("you are same as rating ....");
+// let rating = 3;
+// if (rating === 3) {
+//   console.log("you are same as rating ....");
+// }
+let age = 17;
+
+if (age >= 18) {
+  console.log("adult");
+} else {
+  console.log("minor");
 }
