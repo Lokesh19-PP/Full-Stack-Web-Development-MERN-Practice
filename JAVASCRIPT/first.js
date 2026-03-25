4f4bc5ece8f6201182fa1d95e535260d9d4da919
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-03-25 22:45:53 +0530

    learn about the nested if-else statement in js

--- JAVASCRIPT/first.js
+++ JAVASCRIPT/first.js
@@ -12,18 +12,36 @@
 //   console.log("minor");
 // }
 
-let day = 2;
+// let day = 2;
 
-if (day == 1) {
-  console.log("monday");
-} else if (day == 2) {
-  console.log("tuesday");
-} else if (day == 3) {
-  console.log("wednesday");
-} else if (day == 5) {
-  console.log("friday");
-} else if (day == 6) {
-  console.log("saturday");
+// if (day == 1) {
+//   console.log("monday");
+// } else if (day == 2) {
+//   console.log("tuesday");
+// } else if (day == 3) {
+//   console.log("wednesday");
+// } else if (day == 5) {
+//   console.log("friday");
+// } else if (day == 6) {
+//   console.log("saturday");
+// } else {
+//   console.log("sunday");
+// }
+
+let a = 10,
+  b = 20,
+  c = 25;
+
+if (a > b) {
+  if (a > c) {
+    console.log("a is greater");
+  } else {
+    console.log("c is a greater");
+  }
 } else {
-  console.log("sunday");
+  if (b > c) {
+    console.log("b is greater");
+  } else {
+    console.log("c is greater");
+  }
 }
