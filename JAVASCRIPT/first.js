ccac48c6b6dd0bcb643c30ced6c2635a64b4c7c8
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-03-25 22:33:30 +0530

    learn about the if statement in js

--- JAVASCRIPT/first.js
+++ JAVASCRIPT/first.js
@@ -1,1 +1,6 @@
-console.log("Hello from the first JS file!!!!");
+// console.log("Hello from the first JS file!!!!");
+
+let rating = 3;
+if (rating === 3) {
+  console.log("you are same as rating ....");
+}
