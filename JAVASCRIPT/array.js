938a4a5e7a8f45a04c70069ab23d3aab4751e1f4
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-03-26 23:35:50 +0530

    learn about the array slice and splice methods in JS

--- JAVASCRIPT/array.js
+++ JAVASCRIPT/array.js
@@ -25,6 +25,25 @@ let arr1 = [1, 2, 3, 4, 5];
 let arr2 = [6, 7, 8, 9, 10];
 let arr3 = arr1.concat(arr2);
 console.log(arr3);
+
 console.log(arr3.includes(5));
 console.log(arr3.includes(11));
+
 console.log(arr3.indexOf(7));
+
+let colors = [
+  "red",
+  "yellow",
+  "blue",
+  "violet",
+  "black",
+  "white",
+  "grey",
+  "pink",
+];
+console.log(colors.slice(5));
+console.log(colors.slice(3, 5));
+console.log(colors.slice(-3));
+
+console.log(colors.splice(5, 1));
+console.log(colors);
