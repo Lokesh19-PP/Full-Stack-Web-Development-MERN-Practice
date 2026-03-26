6854318937002ac56efeff6b43dc60a8eb4db401
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-03-26 23:21:19 +0530

    learn about how to create array and initialize the values to the array and learn about hte basic methods of array

---
+++ JAVASCRIPT/array.js
@@ -0,0 +1,22 @@
+let student = ["lokesh", "ayushi", "manish", "ganesh"];
+console.log(student[0]);
+console.log(student[1]);
+console.log(student[2]);
+console.log(student[3]);
+
+student[3] = "tanmay";
+
+console.log(student);
+
+// arrya methods
+
+console.log(student.length);
+
+student.push("riya");
+console.log(student);
+
+student.pop();
+console.log(student);
+
+student.reverse();
+console.log(student);
