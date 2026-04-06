f0c0893082e46ecd85bf24f343a644fd7535e03a
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-04-06 21:17:42 +0530

    learn about the function parameter and argument passing with multiple argument in JS

--- JAVASCRIPT/Function.js
+++ JAVASCRIPT/Function.js
@@ -4,3 +4,21 @@ function display() {
 display();
 display();
 display();
+
+function sum(no1, no2) {
+  console.log(`addition is ${no1 + no2}`);
+}
+sum(10, 20);
+
+function name_dis(firstName, lastName) {
+  console.log(`first name : ${firstName}`);
+  console.log(`last name : ${lastName}`);
+}
+name_dis("Lokesh", "Pawar");
+
+function rant(message) {
+  console.log(message.toUpperCase());
+  console.log(message.toUpperCase());
+  console.log(message.toUpperCase());
+}
+rant("I hate beets");
