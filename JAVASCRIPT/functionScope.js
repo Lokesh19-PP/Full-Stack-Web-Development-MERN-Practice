c0c357502eedbe48357c69a88d66a3e8f9a32654
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-04-11 20:18:20 +0530

    Learn about the funtion Lexical Scope in JS

--- JAVASCRIPT/functionScope.js
+++ JAVASCRIPT/functionScope.js
@@ -1,3 +1,4 @@
+//function scope
 let eggs = 5;
 function egg() {
   let eggs = 6;
@@ -9,3 +10,15 @@ let bird = "sparrow";
 function name() {
   console.log(bird);
 }
+
+//Lexical Scope
+function bank() {
+  const hero = ["spiderman", "superman", "batman"];
+  function cryforhelp() {
+    for (let h of hero) {
+      console.log(h);
+    }
+  }
+  cryforhelp();
+}
+bank();
